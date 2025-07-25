629d8c8f1e5093be5c279dd7eecdf6e7725ab75d
raju kumar <raju.van07@gmail.com>
2025-07-25 19:33:16 +0530

    loan service sample code

---
+++ loan-service/src/main/java/com/example/loanservice/dto/LoanDto.java
@@ -0,0 +1,11 @@
+package com.example.loanservice.dto;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoanDto {
+    private Long id;
+    private String name;
+}
