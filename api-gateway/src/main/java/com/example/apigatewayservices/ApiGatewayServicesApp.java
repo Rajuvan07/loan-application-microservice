a615bc86b4ae9de929a4f4ab8ac7582c7c869af6
Raju <raju.van07@gmail.com>
2025-07-25 19:30:15 +0530

    api gateway code (#2)

---
+++ api-gateway/src/main/java/com/example/apigatewayservices/ApiGatewayServicesApp.java
@@ -0,0 +1,13 @@
+package com.example.apigatewayservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayServicesApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServicesApp.class, args);
+	}
+
+}
